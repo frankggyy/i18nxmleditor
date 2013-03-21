ac610fb8e58d30205a2cbb47a0c23dfcb45eb1ac
Tiger Wang <wanghu-520@163.com>
2013-03-21 10:38:12 +0800

    project initial import

---
+++ src/com/whnec/tool/eclipse/plugin/i18nxmleditor/I18nXmlEditorActivator.java
@@ -0,0 +1,61 @@
+package com.whnec.tool.eclipse.plugin.i18nxmleditor;
+
+import org.eclipse.jface.resource.ImageDescriptor;
+import org.eclipse.ui.plugin.AbstractUIPlugin;
+import org.osgi.framework.BundleContext;
+
+/**
+ * The activator class controls the plug-in life cycle
+ */
+public class I18nXmlEditorActivator extends AbstractUIPlugin {
+
+	// The plug-in ID
+	public static final String PLUGIN_ID = "com.whnec.tool.editor.I18nXmlEditor"; //$NON-NLS-1$
+
+	// The shared instance
+	private static I18nXmlEditorActivator plugin;
+	
+	/**
+	 * The constructor
+	 */
+	public I18nXmlEditorActivator() {
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext)
+	 */
+	public void start(BundleContext context) throws Exception {
+		super.start(context);
+		plugin = this;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext)
+	 */
+	public void stop(BundleContext context) throws Exception {
+		plugin = null;
+		super.stop(context);
+	}
+
+	/**
+	 * Returns the shared instance
+	 *
+	 * @return the shared instance
+	 */
+	public static I18nXmlEditorActivator getDefault() {
+		return plugin;
+	}
+
+	/**
+	 * Returns an image descriptor for the image file at the given
+	 * plug-in relative path
+	 *
+	 * @param path the path
+	 * @return the image descriptor
+	 */
+	public static ImageDescriptor getImageDescriptor(String path) {
+		return imageDescriptorFromPlugin(PLUGIN_ID, path);
+	}
+}
